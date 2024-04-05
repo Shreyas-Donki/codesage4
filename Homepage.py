2799a19edb4d19fea8ce2bcee5d419db472657bf
Shreyas SD <51224755+Shreyas-Donki@users.noreply.github.com>
2024-04-05 14:33:18 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -11,9 +11,10 @@ lan = st.selectbox("#### Language", ["English", "Hindi","Kannada"])
 
 if lan == "English":
     home_title = "CodeSage"
-    home_introduction = "Welcome to CodeSage,."
+    home_introduction = "Welcome to Code Sage, Connect, Collaborate, and Cultivate Your Tech Network  |  Artificial General Intelligence powered Next-Gen suite of automated hiring solutions"
+    with st.sidebar:"
     with st.sidebar:
-        st.markdown(' - V1.0.1')
+        st.markdown('Code Sage - V1.0.1')
         st.markdown(""" 
         #### Let's contact:
         [Shreyas SD](https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile)
@@ -42,10 +43,11 @@ if lan == "English":
     st.image(im, width=100)
     st.markdown(f"""# {home_title} <span style=color:#2E9BF5><font size=5>Beta</font></span>""",unsafe_allow_html=True)
     st.markdown("""\n""")
-    #st.markdown("#### Greetings")
-    st.markdown("Smart AI Interviews - Elevate Your Talent, One Question at a Time.")
+    st.markdown("CodeSage - Elevate Your Talent, One Question at a Time , while being Etheically and accurately Video and Audio Proctored by our Artificial Agents!")
     st.markdown("""\n""")
-    st.markdown("""Our Smart AI Interview feature transforms the recruitment process by leveraging ChatGPT 3.5 Turbo API for conducting dynamic aptitude and technical interviews. Designed to challenge candidates with a range of questions from basic to advanced levels, this system provides immediate, detailed feedback, ensuring a comprehensive assessment of skills and knowledge.
+    st.markdown("CodeSage - Customized Feedback and SWOT analysis for each candidate , with the option of AI generated recommmendations for super fast upskilling.")
+    st.markdown("""\n""")
+    st.markdown("""Our AI powered next Gen  Sage, powered by SIA's(in house trained LLM's) feature transforms the recruitment process by leveraging our in house - next Gen LLM's for conducting dynamic aptitude ,technical interviews , coding continuos assessment. Designed to challenge candidates with a range of questions from basic to advanced levels, this system provides immediate, detailed feedback, ensuring a comprehensive assessment of skills and knowledge.
     Comprehensive Project Review""")
     st.markdown("""\n""")
     st.markdown("""Deep Dive into Your Projects, Uncover True Potential.""")
@@ -63,24 +65,25 @@ if lan == "English":
     st.markdown("""\n""")
     st.markdown("""Connect, Collaborate, and Cultivate Your Tech Network""")
     st.markdown("""Envisioned as the "Factbook" for tech professionals, SkillSage offers a unique space for users to connect, share knowledge, and collaborate. By fostering a community of like-minded individuals, our platform encourages networking and mutual learning, empowering tech professionals to build meaningful connections and advance their careers """)
-
+    st.markdown("""\n""")
+    st.markdown("""Why wait? get started for free!! No Credit card of payment option required for free trail period of 1 month!""")
 
     
 
     
     with st.expander("Updates"):
         st.write("""
-        08/13/2023
-        - Fix the error that occurs when the user input fails to be recorded. """)
+        08/13/2024
+        -Shreyas  Fix the error that occurs when the user input fails to be recorded. """)
     with st.expander("What's coming next?"):
         st.write("""
-        Improved voice interaction for a seamless experience. """)
+       AGI Powered Self sustaining hiring and tech socical media Eco-System """)
     st.markdown("""\n""")
     st.markdown("#### Get started!")
-    st.markdown("Select one of the following screens to start your interview!")
+    st.markdown("Pick One platform  to start your interview!")
     selected = option_menu(
             menu_title= None,
-            options=["Professional", "Resume", "Behavioral","Customize!"],
+            options=["Professional", "Resume", "Behavioral","Coding","Customize!","Soft Skills"],
             icons = ["cast", "cloud-upload", "cast"],
             default_index=0,
             orientation="horizontal",
@@ -88,12 +91,12 @@ if lan == "English":
     if selected == 'Professional':
         st.info("""
             📚In this session, the  will assess your technical skills as they relate to the job description.
-            Note: The maximum length of your answer is 4097 tokens!
+           
             - Each Interview will take 10 to 15 mins.
             - To start a new session, just refresh the page.
             - Choose your favorite interaction style (chat/voice)
             - Start introduce yourself and enjoy！ """)
-        if st.button("Start Interview!"):
+        if st.button("hey Code Sage , Assess me!"):
             switch_page("Professional Screen")
     if selected == 'Resume':
         st.info("""
@@ -125,99 +128,92 @@ if lan == "English":
              - Different tones of voice.
              
              Coming at the end of July""")
-    st.markdown("""\n""")
-    st.markdown("#### Wiki")
-    # st.write('[Click here to view common FAQs, future updates and more!](https://jiatastic.notion.site/wiki-8d962051e57a48ccb304e920afa0c6a8?pvs=4)')
-    #st.write(
-    #        f'<iframe src="https://17nxkr0j95z3vy.embednotionpage.com/AI-Interviewer-Wiki-8d962051e57a48ccb304e920afa0c6a8" style="width:100%; height:100%; min-height:500px; border:0; padding:0;"/>',
-    #        unsafe_allow_html=True,
-    #    )
+    
 
+# if lan ==  '中文':
+#     home_title = "AI面试官"
+#     home_introduction = "欢迎使用 AI 面试官，它能够通过生成式AI帮助您准备面试。"
+#     with st.sidebar:
+#         #### Let's contact:
+#         [Shreyas SD](https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile)
+        
+#         Building the next Gen - Industry Disruptive Suite of Artificial Intelligence
+#         and Machine Learning powered solutions across 
+#         Software | Storage | Retail | E commerce | Ad tech | Energy & Gas |
+#             #### 请填写表格，我们非常希望听到您的反馈：
+#             [Feedback Form](https://docs.google.com/forms/d/13f4q03bk4lD7sKR7qZ8UM1lQDo6NhRaAKv7uIeXHEaQ/edit)
 
-if lan ==  '中文':
-    home_title = "AI面试官"
-    home_introduction = "欢迎使用 AI 面试官，它能够通过生成式AI帮助您准备面试。"
-    with st.sidebar:
-        st.markdown('AI面试管 - V0.1.2')
-        st.markdown(""" 
-            #### 领英:
-            [贾皓翔](https://www.linkedin.com/in/haoxiang-jia/)
+#             #### 使用的技术：
 
-            [王梓丞](https://www.linkedin.com/in/todd-wang-5001aa264/)
-            #### 请填写表格，我们非常希望听到您的反馈：
-            [Feedback Form](https://docs.google.com/forms/d/13f4q03bk4lD7sKR7qZ8UM1lQDo6NhRaAKv7uIeXHEaQ/edit)
+#             [OpenAI](https://openai.com/)
 
-            #### 使用的技术：
+#             [FAISS](https://github.com/facebookresearch/faiss)
 
-            [OpenAI](https://openai.com/)
+#             [Langchain](https://github.com/hwchase17/langchain)
 
-            [FAISS](https://github.com/facebookresearch/faiss)
+#                         """)
+#     st.markdown(
+#         "<style>#MainMenu{visibility:hidden;}</style>",
+#         unsafe_allow_html=True
+#     )
+#     st.image(im, width=100)
+#     st.markdown(f"""# {home_title} <span style=color:#2E9BF5><font size=5>Beta</font></span>""", unsafe_allow_html=True)
 
-            [Langchain](https://github.com/hwchase17/langchain)
-
-                        """)
-    st.markdown(
-        "<style>#MainMenu{visibility:hidden;}</style>",
-        unsafe_allow_html=True
-    )
-    st.image(im, width=100)
-    st.markdown(f"""# {home_title} <span style=color:#2E9BF5><font size=5>Beta</font></span>""", unsafe_allow_html=True)
-
-    st.markdown("""\n""")
-    # st.markdown("#### Greetings")
-    st.markdown(
-        "欢迎使用AI面试官！👏AI面试官是一款由生成式人工智能驱动的个人面试官，可以进行模拟面试。您可以上传您的简历或者复制粘贴工作描述，AI面试官会根据您的情况提出定制化的问题。"
-    )
-    st.markdown("""\n""")
-    with st.expander("更新日志"):
-        st.write("""
-            08/13/2023
-            - 修复了当用户输入失败时的报错问题 """)
-    with st.expander("未来计划"):
-        st.write("""
-            - 提供更加稳定和快速的语音交互
-            - 支持全中文的模拟面试 """)
-    st.markdown("""\n""")
-    st.markdown("#### 让我们开始吧!")
-    st.markdown("请选择以下其中一个开始您的面试！")
-    selected = option_menu(
-        menu_title=None,
-        options=["专业评估", "简历评估", "行为评估"],
-        icons=["cast", "cloud-upload", "cast"],
-        default_index=0,
-        orientation="horizontal",
-    )
-    if selected == '专业评估':
-        st.info("""
-                📚在本次面试中，AI面试官将会根据职位描述评估您的技术能力。
-                注意: 您回答的最大长度为4097个tokens!
-                - 每次面试将会持续10到15分钟。
-                - 您可以通过刷新页面来开始新的面试。
-                - 您可以选择您喜欢的交互方式(文字/语音)
-                - 开始介绍您自己吧！ """)
-        if st.button("开始面试!"):
-            switch_page("Professional Screen")
-    if selected == '简历评估':
-        st.info("""
-                📚在本次面试中，AI面试官将会根据您的简历评估您的过往经历。
-                注意: 您回答的最大长度为4097个tokens!
-                - 每次面试将会持续10到15分钟。
-                - 您可以通过刷新页面来开始新的面试。
-                - 您可以选择您喜欢的交互方式(文字/语音)
-                - 开始介绍您自己吧！ """)
-        if st.button("开始面试!"):
-            switch_page("Resume Screen")
-    if selected == '行为评估':
-        st.info("""
-            📚在本次面试中，AI面试官将会根据您的简历评估您的技术能力。
-            注意: 您回答的最大长度为4097个tokens!
-            - 每次面试将会持续10到15分钟。
-            - 您可以通过刷新页面来开始新的面试。
-            - 您可以选择您喜欢的交互方式(文字/语音)
-            - 开始介绍您自己吧！ """)
-        if st.button("开始面试!"):
-            switch_page("Behavioral Screen")
-    st.markdown("""\n""")
-    st.markdown("#### 维基")
-    st.write(
-        '[点击查看常见问题，更新和计划！](https://jiatastic.notion.site/wiki-8d962051e57a48ccb304e920afa0c6a8?pvs=4)')
+#     st.markdown("""\n""")
+#     # st.markdown("#### Greetings")
+#     st.markdown(
+#         "欢迎使用AI面试官！👏AI面试官是一款由生成式人工智能驱动的个人面试官，可以进行模拟面试。您可以上传您的简历或者复制粘贴工作描述，AI面试官会根据您的情况提出定制化的问题。"
+#     )
+#     st.markdown("""\n""")
+#     with st.expander("更新日志"):
+#         st.write("""
+#             08/13/2023
+#             - 修复了当用户输入失败时的报错问题 """)
+#     with st.expander("未来计划"):
+#         st.write("""
+#             - 提供更加稳定和快速的语音交互
+#             - 支持全中文的模拟面试 """)
+#     st.markdown("""\n""")
+#     st.markdown("#### 让我们开始吧!")
+#     st.markdown("请选择以下其中一个开始您的面试！")
+#     selected = option_menu(
+#         menu_title=None,
+#         options=["专业评估", "简历评估", "行为评估"],
+#         icons=["cast", "cloud-upload", "cast"],
+#         default_index=0,
+#         orientation="horizontal",
+#     )
+#     if selected == '专业评估':
+#         st.info("""
+#                 📚在本次面试中，AI面试官将会根据职位描述评估您的技术能力。
+#                 注意: 您回答的最大长度为4097个tokens!
+#                 - 每次面试将会持续10到15分钟。
+#                 - 您可以通过刷新页面来开始新的面试。
+#                 - 您可以选择您喜欢的交互方式(文字/语音)
+#                 - 开始介绍您自己吧！ """)
+#         if st.button("开始面试!"):
+#             switch_page("Professional Screen")
+#     if selected == '简历评估':
+#         st.info("""
+#                 📚在本次面试中，AI面试官将会根据您的简历评估您的过往经历。
+#                 注意: 您回答的最大长度为4097个tokens!
+#                 - 每次面试将会持续10到15分钟。
+#                 - 您可以通过刷新页面来开始新的面试。
+#                 - 您可以选择您喜欢的交互方式(文字/语音)
+#                 - 开始介绍您自己吧！ """)
+#         if st.button("开始面试!"):
+#             switch_page("Resume Screen")
+#     if selected == '行为评估':
+#         st.info("""
+#             📚在本次面试中，AI面试官将会根据您的简历评估您的技术能力。
+#             注意: 您回答的最大长度为4097个tokens!
+#             - 每次面试将会持续10到15分钟。
+#             - 您可以通过刷新页面来开始新的面试。
+#             - 您可以选择您喜欢的交互方式(文字/语音)
+#             - 开始介绍您自己吧！ """)
+#         if st.button("开始面试!"):
+#             switch_page("Behavioral Screen")
+#     st.markdown("""\n""")
+#     st.markdown("#### 维基")
+#     st.write(
+#         '[点击查看常见问题，更新和计划！](https://jiatastic.notion.site/wiki-8d962051e57a48ccb304e920afa0c6a8?pvs=4)')
